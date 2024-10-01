3081fbe78c3d89a1c6215099b092beae205df954
Bem Silva <vitorfbs.ti@gmail.com>
2024-10-01 19:23:45 -0300

    feat: add controller to identify locations on text

---
+++ visitor-service/src/api/controllers/identify-locations-controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express"
+import IdentifyLocationUsecase from '../../usecases/identifyLocations/identifiy-locations-usecase';
+import identifyLocationsHTTPRequestSchema from "../../validators/http/identify-locations/identify-locations-request";
+import { Controller } from "./controller";
+import { HttpResponse, accepted, badRequest, internalServerError } from "../protocols/http";
+
+export default class IdentifyLocationsController implements Controller {
+  identifyLocationUsecase: IdentifyLocationUsecase
+
+  constructor(identifyLocationUsecase: IdentifyLocationUsecase) {
+    this.identifyLocationUsecase = identifyLocationUsecase
+  }
+  async handle (req: Request, res: Response): Promise<HttpResponse> {
+    try {
+      const body = req.body
+
+      const { error } = identifyLocationsHTTPRequestSchema.validate(body);
+
+      if (error) {
+        return badRequest(error)
+      }
+
+      const result =  await this.identifyLocationUsecase.execute(body)
+
+      return accepted(result)
+    } catch (error) {
+      console.log(error)
+      return internalServerError()
+    }
+  }
+}
