23b49c064e12cd3f14057e5da65c6dc332e1399a
Bem Silva <vitorfbs.ti@gmail.com>
2024-10-01 21:11:40 -0300

    feat: add usecase data interfaces

---
+++ visitor-service/src/interfaces/usecases.ts
@@ -0,0 +1,10 @@
+export interface IdentifyLocationsData {
+  title: string
+  body: string
+}
+
+export interface IdentifyLocationsMessageData {
+  messageUUID: string
+  title: string
+  body: string
+}
